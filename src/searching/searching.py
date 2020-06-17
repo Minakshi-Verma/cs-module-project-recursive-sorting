d981f45c40a7c5de3be9496f0fef6095b5d39c06
Minakshi Verma <mvedwan@gmail.com>
2020-06-16 23:49:39 -0400

    Searching and sorting work!, Tests are passing!!

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,7 +2,29 @@
 def binary_search(arr, target, start, end):
     # Your code here
     #base case
-    
+    # Check base case 
+    if end >= start: 
+  
+        mid = (end + start) // 2  
+        # If element is present at the middle itself 
+        if arr[mid] == target: 
+            return mid 
+  
+        # If element is smaller than mid, then it can only 
+        # be present in left subarray 
+        elif arr[mid] > target: 
+            return binary_search(arr, target,start, (mid-1)) 
+  
+        # Else the element can only be present in right subarray 
+        else: 
+            return binary_search(arr, target, (mid + 1), end)
+  
+    else: 
+        # Element is not present in the array 
+        return -1
+  
+
+
 
 
 # STRETCH: implement an order-agnostic binary search
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -2,46 +2,55 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-
     
+    
+    # Sorting happens here...
+  
+    a = 0 #from arrA
+    b = 0 #from arrB
+    #k from merged_arr
 
-
-
+    for k in range(0, elements):
+        # start comparisons
+        # If a is out of range, push b and iterate.
+        if a >= len(arrA):
+            # we're done with a, push b
+            merged_arr[k] = arrB[b]
+            b += 1
+            
+        # If b is out of range, push a and iterate.
+        elif b >= len(arrB):
+            merged_arr[k] = arrA[a]
+            a += 1
+           
+        # If a is smaller, but it in array and iterate both.
+        elif arrA[a] < arrB[b]:
+            merged_arr[k] = arrA[a]
+            a += 1
+            
+        # If b is smaller put it in array and iterate both.
+        else:          
+            merged_arr[k] = arrB[b]
+            b += 1
+            
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     
     # Your code here
-    if len(arr)>1:
+    if len(arr)>1:  #basecase
+        
         middle= len(arr)//2    #gives the middle of the array
-        arrA= arr[:middle]  # gives the left side of array
-        arrB= arr[middle:]  #gives the right side of the array
+        left= arr[:middle]  # gives the left side of array
+        right= arr[middle:]  #gives the right side of the array
    
-        #recursively call merge_sort() on LHS
-        merge_sort(arrA)
-        #recursively call merge_sort() on RHS        
-        merge_sort(arrB)  
-        i = j = k = 0 
-        #copy data to temp-arrA and arrB 
-        # while i<len(arrA) and j<len(arrB):
-        #     if arrA[i]< arrB[j]:
-        #         arr[k]<arrA[i]
-        #         i+=1
-        #     else:
-        #         arr[k]= arrB[j]
-        #         j+=1
-        #     k+=1 
-        # #Checking if any element was left
-        # while i<len(arrA):
-        #     arr[k]= arrA[i]
-        #     i+= 1
-        #     k+=1
-        # #Checking if any element was right
-        # while j<len(arrB):
-        #     arr[k]= arrB[j]
-        #     j+= 1
-        #     k+=1        
+        #recursively call merge_sort() on Left array
+        L= merge_sort(left)
+        #recursively call merge_sort() on Right array        
+        R= merge_sort(right)  
+       #merge left and right array
+        arr = merge(L,R) 
     return arr
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
